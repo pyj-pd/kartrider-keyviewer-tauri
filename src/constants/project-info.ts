72d56e389f940a8b9ff32d420f3c17f78508fcdf
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2025-02-26 23:43:00 +0900

    add license. add versioning.

--- src/constants/project-info.ts
+++ src/constants/project-info.ts
@@ -1,3 +1,5 @@
+import packageJson from "@/../package.json"
+
 export const githubRepoLink = "https://github.com/pyj-pd/kartrider-keyviewer"
 
-export const programVersion = "v25.02.26-beta"
+export const programVersion = `v${packageJson.version}`
