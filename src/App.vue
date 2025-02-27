076ccf4a673412f2ad0883d111bc6f18fada0a3e
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2025-02-27 21:40:37 +0900

    hide scrollbar. disable right click on production.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue"
+import { onBeforeUnmount, onMounted, ref } from "vue"
 import {
   defaultWebview,
   webviewItems,
@@ -12,6 +12,10 @@ import { initConfigStore } from "./composables/initConfigStore"
 const currentPage = ref<null | WebviewItem>(null)
 const { isConfigLoaded } = initConfigStore()
 
+const cancelRightClick = (event: MouseEvent) => {
+  event.preventDefault()
+}
+
 onMounted(() => {
   // Load webview component
   const label = new URL(window.location.href).hash.substring(1)
@@ -20,6 +24,15 @@ onMounted(() => {
     webviewItems.find((item) => item.label === label) ?? defaultWebview
 
   getCurrentWindow().show()
+
+  // Disable right click
+  if (!import.meta.env.DEV)
+    window.addEventListener("contextmenu", cancelRightClick)
+})
+
+onBeforeUnmount(() => {
+  if (!import.meta.env.DEV)
+    window.removeEventListener("contextmenu", cancelRightClick)
 })
 </script>
 
